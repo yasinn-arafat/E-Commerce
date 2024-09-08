5c366a16255859ed908b03e6e933ad01482fd998
yasinn-arafat <yasinarafat4870@gmail.com>
2024-09-08 21:51:58 +0600

    Header Componenet added

--- src/Pages/Home.jsx/Home.jsx
+++ src/Pages/Home.jsx/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../../Component/HomeComponent/Header";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
